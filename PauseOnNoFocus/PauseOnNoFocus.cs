b1f0e791153f6a76eb6ffec2dbb88a1d0da88229
MrLawbreaker <mrlawbreaker.github@gmail.com>
2015-03-22 02:36:50 +0100

    Initial Commit

---
+++ PauseOnNoFocus/PauseOnNoFocus.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICities;
+using UnityEngine;
+
+namespace PauseOnNoFocus
+{
+    public class Mod : IUserMod
+    {
+
+        public string Description
+        {
+            get { return "Automatically pauses the game when it is not focused"; }
+        }
+
+        public string Name
+        {
+            get { return "Pause on no focus"; }
+        }
+    }
+
+    public class PauseOnUnfocus : LoadingExtensionBase
+    {
+        public override void OnCreated(ILoading loading)
+        {
+            Application.runInBackground = false;
+        }
+    }
+}
